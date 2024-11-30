4b01726a2757f16c3deeb6c063c7664adf39ad3d
Daniel M. Matongo <mmatongo_@hotmail.com>
2024-11-30 12:07:38 +0000

    init logging

---
+++ src/usr/local/emhttp/plugins/cloudflared/scripts/init-logs.sh
@@ -0,0 +1,15 @@
+set -e
+#!/bin/bash
+
+LOG_DIR="/var/log/cloudflared"
+LOGFILE="$LOG_DIR/cloudflared.log"
+
+mkdir -p "$LOG_DIR"
+
+if [ ! -f "$LOGFILE" ]; then
+    touch "$LOGFILE"
+    echo "$(date '+%Y-%m-%d %H:%M:%S') Cloudflared log file initialized." > "$LOGFILE"
+fi
+
+chmod 644 "$LOGFILE"
+chown nobody:users "$LOGFILE"
